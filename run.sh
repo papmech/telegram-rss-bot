ff1d9171796bb50de66fdf4f0b1ff082478805f1
papmech <1094252+papmech@users.noreply.github.com>
2019-12-18 17:42:04 +0800

    Removed the need for git pull in run.sh

--- run.sh
+++ run.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/bin/sh
  
 while :
 do
-    git pull
     python3 telegram-rss-bot.py "$1" --interval 60 &
     TASK_PID=$!
     echo "Got TASK_PID = $TASK_PID"
-    sleep 3600
+    # Restart the bot every 30 minutes
+    sleep 1800
     kill -9 $TASK_PID
 done
